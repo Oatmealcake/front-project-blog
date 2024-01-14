a70e3a5c1f07cd8d5a9bc1d9305b58819e9c0253
Oatmealcake <rlarhdms26@gmail.com>
2024-01-14 20:53:56 +0900

    update : 3-3 기본 구조 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,7 @@
-import './App.css';
-import { Route, Routes, Navigate, Link } from 'react-router-dom'
+import Router from './components/Router';
 
 function App() {
-  return (
-    <>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/posts">Post Link</Link>
-        </li>
-        <li>
-          <Link to="/posts/:id">Post Detail</Link>
-        </li>
-        <li>
-          <Link to="/posts/new">Post New</Link>
-        </li>
-        <li>
-          <Link to="/posts/edit/:id">Post Edit</Link>
-        </li>
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
-      </ul>
-      <Routes>
-        <Route path="/" element={<h1>Home Pages</h1>} />
-        <Route path="/posts" element={<h1>Post List Pages</h1>} />
-        <Route path="/posts/:id" element={<h1>Post Detail Pages</h1>} />
-        <Route path="/posts/new" element={<h1>Post New Pages</h1>} />
-        <Route path="/posts/edit/:id" element={<h1>Post Edit Pages</h1>} />
-        <Route path="/profile" element={<h1>Profile Pages</h1>} />
-        <Route path="*" element={<Navigate replace to="/" />} />
-      </Routes>
-    </>
-  );
+  return <Router />;
 }
 
 export default App;
