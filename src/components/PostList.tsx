885708c529e717ca6e43ca40893e2239b8f980a5
Oatmealcake <rlarhdms26@gmail.com>
2024-01-29 00:04:47 +0900

    udpate : 6-4 카테고리 탭

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -7,10 +7,8 @@ import { toast } from 'react-toastify';
 
 interface PostListProps {
   hasNavigation?: boolean;
-  defaultTab?: TabType;
+  defaultTab?: TabType | CategoryType;
 }
-
-type TabType = "all" | "my";
 
 export interface PostProps {
   id?: string;
@@ -21,13 +19,19 @@ export interface PostProps {
   createdAt: string;
   updatedAt?: string;
   uid: string;
+  category?: CategoryType;
 }
+
+type TabType = "all" | "my";
+
+export type CategoryType = 'Frontend' | 'Backend' | 'Web' | 'Native';
+export const CATEGORIES: CategoryType[] = ['Frontend', 'Backend', 'Web', 'Native'];
 
 export default function PostList({
   hasNavigation = true,
   defaultTab = 'all',
 }: PostListProps) {
-  const [activeTab, setActiveTab] = useState<TabType>(defaultTab);
+  const [activeTab, setActiveTab] = useState<TabType | CategoryType>(defaultTab);
   const [posts, setPosts] = useState<any[]>([]);
   const { user } = useContext(AuthContext);
 
@@ -43,9 +47,16 @@ export default function PostList({
         where('uid', '==', user.uid),
         orderBy('createdAt', 'asc')
       );
-    } else {
+    } else if(activeTab === 'all') {
       // 모든 글 보여주기
       postsQuery = query(postsRef, orderBy('createdAt', 'asc'));
+    } else {
+      // 카테고리 글 보여주기
+      postsQuery = query(
+        postsRef,
+        where('category', '==', activeTab),
+        orderBy('createdAt', 'asc')
+      );
     }
     const datas = await getDocs(postsQuery);
     datas?.forEach((doc) => {
@@ -85,6 +96,16 @@ export default function PostList({
           >
             나의 글
           </div>
+          {CATEGORIES?.map((category) => (
+            <div
+              key={category}
+              role="presentation"
+              onClick={() => setActiveTab(category)}
+              className={activeTab === category ? 'post__navigation--active' : ''}
+            >
+              {category}
+            </div>
+          ))}
         </div>
       )}
       <div className="post__list">
